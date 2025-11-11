6ea8c2387185cb85981f18cedda80322ecc2f670
Pavel Yakovlev <today_na@mail.ru>
2025-11-11 13:34:05 +0300

    -Поправил кодировку файла

--- API/src/main/java/services/ItemService.java
+++ API/src/main/java/services/ItemService.java
@@ -7,30 +7,31 @@ import utils.db.DbUtils;
 import java.util.List;
 
 public class ItemService {
+
     public ItemService() {
     }
 
-    @Step("D?D���D1���,D, ���?���������%D���D_���?���,���O D���D_ ID: {id}")
+    @Step("Поиск сущности в БД по id: {id}")
     public Item findEntity(int id) {
         return DbUtils.findById(Item.class, id);
     }
 
-    @Step("D�D_�.�?D�D�D�D�D,D� �?���%D�D_�?�,D, D� D`D\"")
+    @Step("Сохранение сущности в БД")
     public void saveEntity(Item item) {
         DbUtils.save(item);
     }
 
-    @Step("D���D'D���D���D,���,���O ���?���������%D���D_���?���,���O D,D��� D`D\"")
+    @Step("Удаление сущности из БД")
     public void deleteEntity(Item item) {
         DbUtils.delete(item);
     }
 
-    @Step("DzD���D���D_D���D,���,���O ���?���������%D���D_���?���,���O D��� D`D\"")
+    @Step("Обновление сущности в БД")
     public void updateEntity(Item item) {
         DbUtils.update(item);
     }
 
-    @Step("D?D���D1���,D, D������?D���. D���D_D������OD���D_D���D������,D���D���D���D1 D��� D`D\"")
+    @Step("Получение списка всех сущностей из БД")
     public List<Item> findAllEntities() {
         return DbUtils.findAll(Item.class);
     }
